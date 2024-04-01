568d312213b1a59c6fbd24c8b6fcc7072aa08731
79AceVo <96391296+79AceVo@users.noreply.github.com>
2024-03-31 21:49:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,50 +18,53 @@ prompt = PromptTemplate(
     AI:'''
 )
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
-llm = ChatOpenAI(openai_api_key=openai_api_key)
-memory = ConversationBufferWindowMemory(memory_key="chat_history", k=4)
-llm_chain =  LLMChain(
-llm=llm,
-memory=memory,
-prompt=prompt
-)
-
-
-st.set_page_config(
-    page_title="ChatGPT Clone",
-    page_icon="🤖",
-    layout="wide"
-)
-
-
-st.title("ChatGPT Clone")
-
-
-# check for messages in session and create if not exists
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [
-        {"role": "assistant", "content": "Hello there, am ChatGPT clone"}
-    ]
-
-
-# Display all messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-
-
-user_prompt = st.chat_input()
-
-if user_prompt is not None:
-    st.session_state.messages.append({"role": "user", "content": user_prompt})
-    with st.chat_message("user"):
-        st.write(user_prompt)
-
-
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Loading..."):
-            ai_response = llm_chain.predict(question=user_prompt)
-            st.write(ai_response)
-    new_ai_message = {"role": "assistant", "content": ai_response}
-    st.session_state.messages.append(new_ai_message)
+if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+if openai_api_key.startswith('sk-'):
+    llm = ChatOpenAI(openai_api_key=openai_api_key)
+    memory = ConversationBufferWindowMemory(memory_key="chat_history", k=4)
+    llm_chain =  LLMChain(
+    llm=llm,
+    memory=memory,
+    prompt=prompt
+    )
+    
+    
+    st.set_page_config(
+        page_title="ChatGPT Clone",
+        page_icon="🤖",
+        layout="wide"
+    )
+    
+    
+    st.title("ChatGPT Clone")
+    
+    
+    # check for messages in session and create if not exists
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Hello there, am ChatGPT clone"}
+        ]
+    
+    
+    # Display all messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+    
+    
+    user_prompt = st.chat_input()
+    
+    if user_prompt is not None:
+        st.session_state.messages.append({"role": "user", "content": user_prompt})
+        with st.chat_message("user"):
+            st.write(user_prompt)
+    
+    
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Loading..."):
+                ai_response = llm_chain.predict(question=user_prompt)
+                st.write(ai_response)
+        new_ai_message = {"role": "assistant", "content": ai_response}
+        st.session_state.messages.append(new_ai_message)
