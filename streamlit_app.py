4e309a89afe1c4370aad6dbc940f227985443438
79AceVo <96391296+79AceVo@users.noreply.github.com>
2024-03-31 21:52:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from langchain.memory import ConversationBufferWindowMemory
 st.title("ChatGPT Clone")
 prompt = PromptTemplate(
     input_variables=["chat_history", "question"],
-    template='''You are a very kindl and friendly AI assistant. You are
+    template='''You are a very kind and friendly AI assistant. You are
     currently having a conversation with a human. Answer the questions
     in a kind and friendly tone with some sense of humor.
     
